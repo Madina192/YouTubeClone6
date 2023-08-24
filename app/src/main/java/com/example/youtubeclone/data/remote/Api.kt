c4ac9e71b0a4761d9c9a0f9fd73004ebe24891e1
Madina <madinaakpaeva000@gmail.com>
2023-08-24 19:06:25 +0600

    changed smth

--- app/src/main/java/com/example/youtubeclone/data/remote/Api.kt
+++ app/src/main/java/com/example/youtubeclone/data/remote/Api.kt
@@ -1,7 +1,6 @@
 package com.example.youtubeclone.data.remote
 
 import com.example.youtubeclone.data.model.Playlist
-import org.koin.android.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,7 +10,7 @@ interface Api {
     fun getPlaylists(
         @Query("part") part: String = "contentDetails, snippet",
         @Query("channelId") channelId: String = "UCFiaumQAjtZwWlxdbgYP7vw",
-        @Query("key") key: String = BuildConfig.API_KEY,
+        @Query("key") key: String = com.example.youtubeclone.BuildConfig.API_KEY,
         @Query("maxResults") maxResults: Int = 10,
     ) : Call<Playlist>
 }
--- app/src/main/java/com/example/youtubeclone/data/remote/Retrofit.kt
+++ app/src/main/java/com/example/youtubeclone/data/remote/Retrofit.kt
@@ -1,11 +1,10 @@
 package com.example.youtubeclone.data.remote
 
-import org.koin.android.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Retrofit {
-    var retrofit = Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+    var retrofit = Retrofit.Builder().baseUrl(com.example.youtubeclone.BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create()).build()
 
     var api = retrofit.create(Api::class.java)
